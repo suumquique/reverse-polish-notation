71c81b50b457f8b2d91052aa487248f59448b84d
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-05-01 21:46:03 +0300

    Added main project file, included headers

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+// Математические операторы, которые могут встретиться в выражении
+const char operators[] = { '-', '+', '/', '*', '%', '^' };
+
+int main(void) {
+
+}
