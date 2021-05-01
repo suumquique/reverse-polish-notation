0efdc114d38029ae63839d14aea49c226f96c259
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-05-01 21:54:34 +0300

    Add reading of the initial math expression

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 
 //  одировка консоли Windows-1251 дл€ корректного ввода и вывода русских символов
 #define RUS_ENCODING 1251
+// ћаксимальна€ длина подаваемого на вход математического выражени€
+#define MAX_EXPRESSION_LENGTH 2600
 
 // ћатематические операторы, которые могут встретитьс€ в выражении
 const char operators[] = { '-', '+', '/', '*', '%', '^' };
@@ -17,6 +19,9 @@ int main(void) {
 	SetConsoleCP(RUS_ENCODING);
 	SetConsoleOutputCP(RUS_ENCODING);
 	
+	char expression[MAX_EXPRESSION_LENGTH] = { 0 };
+	puts("¬ведите корректное математическое выражение в стандартной (инфиксной) форме:\n");
+	gets_s(expression, MAX_EXPRESSION_LENGTH);
 }
 
 /*¬ качестве аргументов должны передаватьс€ операторы из списка допустимых операторов, в ином случае поведение не определено.
