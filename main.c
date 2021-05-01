be1dd4ec826483d3ee010fb80778a5ca16b1a97e
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-05-01 21:46:48 +0300

    Add a function that determines the priority of operators

--- main.c
+++ main.c
@@ -7,6 +7,15 @@
 // Математические операторы, которые могут встретиться в выражении
 const char operators[] = { '-', '+', '/', '*', '%', '^' };
 
+bool isOperatorMorePrioritized(char firstOperator, char secondOperator);
+
 int main(void) {
 
+}
+
+/*В качестве аргументов должны передаваться операторы из списка допустимых операторов, в ином случае поведение не определено.
+Если приоритет первого оператора выше (например, первый оператор - умножение, а второй - сложение), возвращается истина; иначе - ложь.*/
+bool isOperatorMorePrioritized(char firstOperator, char secondOperator) {
+	// Если первой оператор находится в списке операторов позднее, значит, имеет больший приоритет
+	return strchr(operators, firstOperator) > strchr(operators, secondOperator);
 }
